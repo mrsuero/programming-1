02eb83b9ddc32ff10385f846fffbf03360da2861
Khac Uy Pham <164907812+mrsuero@users.noreply.github.com>
2024-10-22 11:40:07 +0700

    10423122 reverse_array.c

--- lab_w5/reverse_array.c
+++ lab_w5/reverse_array.c
@@ -1,19 +1,31 @@
 #include <stdlib.h>
+#include <stdio.h>
 
-// define your swap function here
-
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
 // reverse array function
 void reverse_array(int *arr, int N)
 {
-    // your code here
-
-    // use your swap function
+    for (int i = 0; i < N/2; i++)
+    {
+        swap(&arr[i], &arr[N - 1 - i]);
+    }
 }
 
 int main()
 {
     int arr[] = {1, 4, 3, 7, 6};
-    reverse_array(arr, 5);
+    int N = sizeof(arr)/sizeof(arr[0]);
+    reverse_array(arr, N);
+
+    for (int i = 0; i < N; i++)
+    {
+        printf("%d ", arr[i]);
+    }
 
     return 0; // set break point
 }
