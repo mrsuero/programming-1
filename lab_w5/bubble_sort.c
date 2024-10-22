2b3c680c642e0d7898a480297cd45b29b58a976e
Khac Uy Pham <164907812+mrsuero@users.noreply.github.com>
2024-10-22 12:01:10 +0700

    10423122 bubble_sort.c

--- lab_w5/bubble_sort.c
+++ lab_w5/bubble_sort.c
@@ -1,14 +1,49 @@
-// copy your swap function from reverse_array.c
+#include <stdlib.h>
+#include <stdio.h>
+
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
 
 void bubble_sort(int *arr, int N)
 {
+    for (int step = 0; step < N - 1; ++step)
+    {
+        for (int i = 0; i < N - step - 1; i++)
+        {
+            if (arr[i] > arr[i + 1])
+            {
+                int temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+            }
+        }
+        if (swap == 0)
+        {
+            break;
+        }
+    }
+}
 
+void print_arr(int *arr, int N)
+{
+    for (int i = 0; i < N; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
 }
 
 int main()
 {
     int arr[] = {1, 4, 3, 7, 6};
-    bubble_sort(arr, 5);
+    int N = sizeof(arr)/sizeof(arr[0]);
+    bubble_sort(arr, N);
 
-    return 0; // set break point
+    print_arr(arr, N);
+
+    return 0;
 }
